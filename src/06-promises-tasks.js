f379e7bcca6c72247b782dbcd28fd1fcfbc9e941
Zabashta_Artur <zarpost@gmail.com>
2022-12-29 16:31:45 +0300

    feat: add solution of the last task of block 06-promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -107,8 +107,38 @@ function getFastestPromise(array) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+
+function myPromiseAll(taskList) {
+  // to store results
+  const results = [];
+  // to track how many promises have completed
+  let promisesCompleted = 0;
+
+  // return new promise
+  return new Promise((resolve, reject) => {
+    taskList.forEach((promise, index) => {
+      // if promise passes
+      promise.then((val) => {
+        // store its outcome and increment the count
+        results[index] = val;
+        promisesCompleted += 1;
+        // if all the promises are completed,
+        // resolve and return the result
+        if (promisesCompleted === taskList.length) {
+          resolve(results);
+        }
+      })
+        // if any promise fails, reject.
+        .catch((error) => { reject(error); });
+    });
+  });
+}
+
+function chainPromises(array, action) {
+  // throw new Error('Not implemented');
+  return (myPromiseAll(array)
+    .then((result) => result.reduce((acc, cur) => action(acc, cur)),
+      (error) => error));
 }
 
 module.exports = {
