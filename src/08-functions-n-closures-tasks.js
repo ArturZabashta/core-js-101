8c2fc70f84d1c10388267e5b503054bde2712beb
Zabashta_Artur <zarpost@gmail.com>
2023-01-03 02:39:27 +0300

    feat: add part of the last task  of functions-n-closures module

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -155,8 +155,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argument = Array.from(args).pop();
+    logFunc(`${func.name}(${argument}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argument}) ends`);
+    return result;
+  };
 }
 
 
